9897923b7c6b97130165709fc1ce6b922d0beee5
SRISHANTH P M <srishanth232007@gmail.com>
2025-05-16 21:06:48 +0530

    Variable assignment, String and Numbers.

--- Variables and Data Types/script.js
+++ Variables and Data Types/script.js
@@ -1,2 +1,53 @@
-console.log("Welcome to variables");
+// // VARIBLES AND DATA TYPES
+
+
+// // VARIBLES
+
+// var variableName = 'Welcome to Variables';
+// variableName = 'Welcome to the section: Variables';
+// let userName = 'Sri'
+// const age = 18;
+
+// console.log(variableName);
+// console.log(userName);
+// console.log(age);
+
+
+
+// DATA TYPES
+
+
+// STRING
+
+// const singleQuotes = 'This is a string with single quotes';
+// const doubleQuotes = "This is a string with double quotes";
+// const backticks = `This is a string with backticks`;
+// console.log(singleQuotes);
+// console.log(doubleQuotes);
+// console.log(backticks);    // console.log(typeof backticks);  --> string
+
+// const userName = 'Sri';
+// const greeting = `Hello, ${userName}!`;
+// console.log(greeting);
+
+// const parsing = `Age: ${17 + 1}`;
+// console.log(parsing);
+
+
+// NUMBERS
+
+// const firstNumber = 10;
+// const secondNumber = 20;
+// console.log(firstNumber);
+// console.log(secondNumber);
+
+// result = firstNumber + secondNumber;
+// console.log(result);
+
+// const string = 'Hello';
+// result = string / secondNumber;
+// console.log(result);
+
+
+// BOOLEAN
 
