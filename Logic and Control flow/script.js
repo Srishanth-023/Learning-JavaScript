5db8a5a8f6facd61ac9f39c4a6c66450964375a3
SRISHANTH P M <srishanth232007@gmail.com>
2025-05-16 23:26:39 +0530

    Logic and Control flow.

---
+++ Logic and Control flow/script.js
@@ -0,0 +1,29 @@
+console.log("Welcome to the section: Logic and Control flow");
+
+// CONDITIONALS 
+
+const age = 18;
+
+if (age > 18){
+    console.log('Welcome to the club !');
+} else if (age === 18){
+    console.log('You just turned 18, Welcome to the club !!');
+} else {
+    console.log('Sorry, you are not allowed to enter the club');
+}
+
+
+// LOOPS
+
+// FOR LOOP
+for (let i = 1; i <= 10; i++){
+    console.log(i);
+}
+
+// WHILE LOOP
+j = 1;
+
+while (j <= 10){
+    console.log(j);
+    j++;
+}
