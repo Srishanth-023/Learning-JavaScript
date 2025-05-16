4e78b386145fa62a9137154f0931174388c5e8f9
SRISHANTH P M <srishanth232007@gmail.com>
2025-05-16 22:41:53 +0530

    Operators.

---
+++ Operations/script.js
@@ -0,0 +1,47 @@
+console.log("Welcome to the section: Operations");
+
+// // COMPARISON OPERATORS
+
+// const num1 = 10;
+// const num2 = 15;
+
+// console.log(num1 > num2);
+// console.log(num1 >= num2);
+// console.log(num1 < num2);
+// console.log(num1 <= num2);
+// console.log(num1 == num2); 
+// console.log(num1 != num2);
+// console.log(num1 === num2);
+// console.log(num1 !== num2);
+
+// // STRICT EQUALITY AND INEQUALITY --> Compares both value and type
+// console.log(5 === 5);
+// console.log(5 === '5'); // false
+
+// // LOOSE EQUALITY AND INEQUALITY --> Compares only values
+// console.log(5 == 5);
+// console.log(5 == '5'); // true
+// // Examples of misbehaviour of loose equality
+// console.log(0 == false); // true
+// console.log(0 == ''); // true
+// console.log('0' == ''); // false
+// console.log(' ' == false); // true
+// console.log(false == 'flase'); // false
+
+
+// // LOGICAL OPERATORS
+
+// // AND
+// console.log(true && true);
+// console.log(true && false);
+// console.log(true && false && true);
+
+// // OR
+// console.log(true || true);
+// console.log(true || false);
+// console.log(false || false);
+// console.log(true || false || true);
+
+// // NOT
+// console.log(!true);
+// console.log(!false);
